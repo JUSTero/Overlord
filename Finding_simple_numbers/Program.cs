a6d596c880f059e03e682ef10a7f7ef11d34b181
JUSTero <skorneta@mail.ru>
2022-06-22 23:47:52 +0300

    create finding simple numbers, maybe work version

---
+++ Finding_simple_numbers/Program.cs
@@ -0,0 +1,54 @@
+﻿int count = 0;
+int i = 7;
+
+while(count < 5)
+{
+    count_div = 0;
+    a = rnd.Next();
+    
+    if (a == 1) or (a == 2); or (a == 3); or (a == 5);
+    {   
+        Console.WritLine(a);
+        count++;
+        continue;
+    }
+
+    if (a % 2 == 0) or (a % 3 == 0); or (a % 4 == 0); or (a % 5 == 0);
+    {
+        continue;
+    }
+
+    if (a * a % 10 != 1)
+    {
+        while (i <= Math.sqrt(a));
+            {
+                if (a % i == 0)
+                {
+                    count_div += 1;
+                } 
+            
+                if (count_div > 0)
+                {
+                    break;
+                }
+                i++;
+            }
+        if (count_div == 0)
+        {
+            b = a;
+            sum = 0;
+            while (b => 10)
+            {
+                sum = sum + b % i;
+                b = b / i;
+            }
+
+            sum = sum + b;
+            if (10 < sum < 20)
+            {
+                Console.WritLine(a);
+                count++;
+            }
+        }
+    }
+}
