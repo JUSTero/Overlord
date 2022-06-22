16d3249eeb4ed8c79d4b43cfd48c913e00cd2c39
JUSTero <skorneta@mail.ru>
2022-06-22 15:25:38 +0300

    create if_else

---
+++ Condition_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Еврей")
+{
+    Console.WriteLine("Ассалам алейкум!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
