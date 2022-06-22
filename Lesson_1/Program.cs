c873a9322803bac6b1b2c6a3b2d6ee11024f604b
JUSTero <skorneta@mail.ru>
2022-06-22 14:31:53 +0300

    ignore .gitignore

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// First programm on C#
-Console.WriteLine("Введите ваше имя");
+Console.Write("Введите ваше имя ");
 string username = Console.ReadLine();
-Console.WriteLine("Привет, ");
-Console.WriteLine(username);
+Console.Write("Привет, ");
+Console.Write(username);
